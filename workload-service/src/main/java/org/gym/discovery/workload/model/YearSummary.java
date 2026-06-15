e565d436c7865e891a5db891e09d7e8da5f3a757
Denys <deniska2002kulikov@gmail.com>
2026-06-15 13:58:21 +0300

    GIA-239 Implement Workload microservice - add models

---
+++ workload-service/src/main/java/org/gym/discovery/workload/model/YearSummary.java
@@ -0,0 +1,13 @@
+package org.gym.discovery.workload.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class YearSummary {
+    private Integer year;
+    private List<MonthSummary> months;
+}
---
+++ workload-service/src/test/java/org/gym/discovery/workload/WorkloadServiceTests.java
@@ -0,0 +1,18 @@
+package org.gym.discovery.workload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+@SpringBootTest
+class WorkloadServiceTests {
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void mainRuns() {
+        assertDoesNotThrow(() -> WorkloadService.main(new String[]{}));
+    }
+}
