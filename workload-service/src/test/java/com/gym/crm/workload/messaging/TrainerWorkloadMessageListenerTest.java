10561abafc63301d9ef840392b40ffdf6d817ce4
Denys <deniska2002kulikov@gmail.com>
2026-06-30 10:49:44 +0300

    GIA-299 Fix sonar comments

--- workload-service/src/test/java/com/gym/crm/workload/messaging/TrainerWorkloadMessageListenerTest.java
+++ workload-service/src/test/java/com/gym/crm/workload/messaging/TrainerWorkloadMessageListenerTest.java
@@ -17,10 +17,8 @@ import java.time.LocalDate;
 import java.time.Month;
 
 import static org.assertj.core.api.Assertions.assertThatNoException;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.contains;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.never;
