6898f8622490938eb548af932f6e117876e036cc
Denys <deniska2002kulikov@gmail.com>
2026-07-02 19:35:51 +0300

    GIA-308 Revert commit with revert and revert error handling for workload messages
    This reverts commit 8b264d725fd35eb37d59211700ddcd5aa6e8564b.

--- workload-service/src/main/java/com/gym/crm/workload/exception/InvalidWorkloadMessageException.java
+++
@@ -1,7 +0,0 @@
-package com.gym.crm.workload.exception;
-
-public class InvalidWorkloadMessageException extends RuntimeException {
-    public InvalidWorkloadMessageException(String message) {
-        super(message);
-    }
-}
--- workload-service/src/main/java/com/gym/crm/workload/exception/WorkloadMessageProcessingException.java
+++
@@ -1,7 +0,0 @@
-package com.gym.crm.workload.exception;
-
-public class WorkloadMessageProcessingException extends RuntimeException {
-    public WorkloadMessageProcessingException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
