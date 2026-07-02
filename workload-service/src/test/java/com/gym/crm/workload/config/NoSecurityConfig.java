ade798568cba867d87f157cc4f8cd662f59861f6
Denys <deniska2002kulikov@gmail.com>
2026-07-02 11:35:01 +0300

    GIA-303 Fix tests

---
+++ workload-service/src/test/java/com/gym/crm/workload/config/NoSecurityConfig.java
@@ -0,0 +1,18 @@
+package com.gym.crm.workload.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@TestConfiguration
+public class NoSecurityConfig {
+    @Bean
+    SecurityFilterChain testSecurity(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .build();
+    }
+}
