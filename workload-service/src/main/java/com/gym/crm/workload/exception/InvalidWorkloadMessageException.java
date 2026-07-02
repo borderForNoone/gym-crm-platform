e3d1a4fb708228c6f11716f00cd27424b12af25c
Denys <deniska2002kulikov@gmail.com>
2026-07-02 20:15:57 +0300

    GIA-299 Implement error handling for workload messages

---
+++ workload-service/src/main/java/com/gym/crm/workload/exception/InvalidWorkloadMessageException.java
@@ -0,0 +1,7 @@
+package com.gym.crm.workload.exception;
+
+public class InvalidWorkloadMessageException extends RuntimeException {
+    public InvalidWorkloadMessageException(String message) {
+        super(message);
+    }
+}
