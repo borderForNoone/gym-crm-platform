4ccfeebcb484ad3e15393e0514e5abc203124823
Denys <deniska2002kulikov@gmail.com>
2026-07-02 12:03:06 +0300

    GIA-303 Fix sonar comment

--- workload-service/src/main/java/com/gym/crm/workload/config/SecurityConfig.java
+++ workload-service/src/main/java/com/gym/crm/workload/config/SecurityConfig.java
@@ -20,8 +20,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(csrf -> csrf.ignoringRequestMatchers("/api/**"))
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(SWAGGER_ENDPOINTS).permitAll()
                         .anyRequest().authenticated())
