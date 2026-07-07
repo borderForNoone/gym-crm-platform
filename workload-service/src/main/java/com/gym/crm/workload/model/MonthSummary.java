794259f89e0d0add85f40bbd8ad448f21204dfe6
Denys <deniska2002kulikov@gmail.com>
2026-07-07 15:01:52 +0300

    GIA-324 Create trainer workload MongoDB schema

--- workload-service/src/main/java/com/gym/crm/workload/model/MonthSummary.java
+++ workload-service/src/main/java/com/gym/crm/workload/model/MonthSummary.java
@@ -3,12 +3,17 @@ package com.gym.crm.workload.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class MonthSummary {
+    @Field("month")
     private Integer month;
+    @Field("trainingSummaryDuration")
     private Integer trainingSummaryDuration;
 }
