75989f98076ce15eaff485336ff7e377442aa065
Denys Kulikov <115070319+borderForNoone@users.noreply.github.com>
2026-07-08 09:57:31 +0300

    GIA-324 Create trainer workload MongoDB schema
    * Reapply "GIA-299 Implement error handling for workload messages"
    
    This reverts commit f8700d5aeba02eb1e89d8814dec5ca175910e163.
    
    * GIA-324 Create trainer workload MongoDB schema

--- workload-service/src/main/java/com/gym/crm/workload/model/TrainerWorkload.java
+++ workload-service/src/main/java/com/gym/crm/workload/model/TrainerWorkload.java
@@ -2,18 +2,31 @@ package com.gym.crm.workload.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.util.List;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "trainer_workloads")
+@CompoundIndex(name = "trainer_name_idx", def = "{'trainerFirstName': 1, 'trainerLastName': 1}")
 public class TrainerWorkload {
-
+    @Indexed(unique = true)
+    @Field("trainerUsername")
     private String trainerUsername;
+    @Field("trainerFirstName")
     private String trainerFirstName;
+    @Field("trainerLastName")
     private String trainerLastName;
+    @Field("isActive")
     private Boolean isActive;
+    @Field("years")
     private List<YearSummary> years;
 }
--- workload-service/src/main/java/com/gym/crm/workload/model/YearSummary.java
+++ workload-service/src/main/java/com/gym/crm/workload/model/YearSummary.java
@@ -2,12 +2,17 @@ package com.gym.crm.workload.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class YearSummary {
+    @Field("year")
     private Integer year;
+    @Field("months")
     private List<MonthSummary> months;
 }
