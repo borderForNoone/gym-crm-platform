7b3a24b592a0ed1f71a6edaabcb3798d21650281
Denys Kulikov <115070319+borderForNoone@users.noreply.github.com>
2026-07-09 11:37:25 +0300

    GIA-325 Implement MongoDB repository for trainer workload (#27)
    * Reapply "GIA-299 Implement error handling for workload messages"
    
    This reverts commit f8700d5aeba02eb1e89d8814dec5ca175910e163.
    
    * GIA-325 Implement MongoDB repository for trainer workload
    
    * GIA-325 Fix imports
    
    * GIA-325 Fix comment

--- workload-service/src/main/java/com/gym/crm/workload/model/TrainerWorkload.java
+++ workload-service/src/main/java/com/gym/crm/workload/model/TrainerWorkload.java
@@ -4,8 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.CompoundIndex;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
@@ -18,7 +18,7 @@ import java.util.List;
 @Document(collection = "trainer_workloads")
 @CompoundIndex(name = "trainer_name_idx", def = "{'trainerFirstName': 1, 'trainerLastName': 1}")
 public class TrainerWorkload {
-    @Indexed(unique = true)
+    @Id
     @Field("trainerUsername")
     private String trainerUsername;
     @Field("trainerFirstName")
