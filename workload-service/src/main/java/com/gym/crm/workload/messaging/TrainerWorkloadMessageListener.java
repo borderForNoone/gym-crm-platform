6dd5bfb739657563e4a6e316ca6f7f7f3dec11d0
Denys <deniska2002kulikov@gmail.com>
2026-07-12 14:23:01 +0300

    GIA-329 Corrections in accordance with the requirements

--- workload-service/src/main/java/com/gym/crm/workload/messaging/TrainerWorkloadMessageListener.java
+++ workload-service/src/main/java/com/gym/crm/workload/messaging/TrainerWorkloadMessageListener.java
@@ -4,6 +4,7 @@ import com.gym.crm.workload.exception.InvalidWorkloadMessageException;
 import com.gym.crm.workload.exception.WorkloadMessageProcessingException;
 import com.gym.crm.workload.service.TrainerWorkloadService;
 import gym.crm.platform.workload.openapi.TrainerWorkloadRequest;
+import io.micrometer.common.util.StringUtils;
 import jakarta.jms.JMSException;
 import jakarta.jms.Message;
 import lombok.RequiredArgsConstructor;
@@ -14,7 +15,6 @@ import org.springframework.stereotype.Component;
 
 import java.time.Clock;
 import java.time.LocalDate;
-import java.util.Objects;
 
 @Slf4j
 @Component
@@ -61,14 +61,14 @@ public class TrainerWorkloadMessageListener {
             throw new InvalidWorkloadMessageException("Request is null");
         }
 
-        String username = request.getTrainerUsername();
-        Objects.requireNonNull(username, "trainerUsername is required");
-        if (username.isBlank()) {
+        if (StringUtils.isBlank(request.getTrainerUsername())) {
             throw new InvalidWorkloadMessageException("trainerUsername cannot be blank");
         }
 
         LocalDate trainingDate = request.getTrainingDate();
-        Objects.requireNonNull(trainingDate, "trainingDate is required");
+        if (trainingDate == null) {
+            throw new InvalidWorkloadMessageException("trainingDate is required");
+        }
         if (trainingDate.isAfter(LocalDate.now(clock))) {
             throw new InvalidWorkloadMessageException("trainingDate cannot be in the future");
         }
