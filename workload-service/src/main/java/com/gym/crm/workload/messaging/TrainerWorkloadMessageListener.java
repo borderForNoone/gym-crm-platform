7da38908b4c2e03f99e0d413f443088178ba9fab
Denys <deniska2002kulikov@gmail.com>
2026-07-17 10:40:16 +0300

    GIA-345 Fix sonar comments

--- workload-service/src/main/java/com/gym/crm/workload/messaging/TrainerWorkloadMessageListener.java
+++ workload-service/src/main/java/com/gym/crm/workload/messaging/TrainerWorkloadMessageListener.java
@@ -40,7 +40,7 @@ public class TrainerWorkloadMessageListener {
             log.info("Received workload event trainer={} action={} txId={}", request != null ? request.getTrainerUsername() : null,
                     request != null ? request.getActionType() : null, transactionId);
             validate(request);
-            process(request, transactionId);
+            process(request);
         } catch (InvalidWorkloadMessageException | WorkloadMessageProcessingException e) {
             handleToDlq(request, transactionId, e.getMessage());
         } catch (Exception e) {
@@ -51,7 +51,7 @@ public class TrainerWorkloadMessageListener {
         }
     }
 
-    private void process(TrainerWorkloadRequest request, String transactionId) {
+    private void process(TrainerWorkloadRequest request) {
         try {
             trainerWorkloadService.updateTrainerWorkload(request);
         } catch (Exception e) {
